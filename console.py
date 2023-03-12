d373b1437826d33797bfaddbcb1ede9224bdf5c3
Hope_Davids <hopedavids@gmail.com>
2023-03-12 14:05:36 -0400

    added docs to console

--- console.py
+++ console.py
@@ -5,15 +5,18 @@ import cmd
 
 
 class BackendShell(cmd.Cmd):
+    """This is a the backend interactive shell interface"""
     intro = "<<Backend Interactive Console. Type help or ? to list commands>>"
     prompt = "(hbnb) "
     file = None
 
     def do_EOF(self, line):
+        # An End of File command to close the open file
         'End of file of the application'
         return True
 
     def do_quit(self, arg):
+        # A Quit command to close the shell program
         'Quit the application here'
         return True
 

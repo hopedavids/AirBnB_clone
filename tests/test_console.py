f8db03e11b280349dc4c1106198b4956b122f689
Hope_Davids <hopedavids@gmail.com>
2023-03-13 06:08:23 -0400

    modified - unittest for the console

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,8 +7,10 @@ from console import BackendShell
 
 
 class TestConsole(unittest.TestCase):
-
+    ''' This class contains definitions for the console unit tests cases'''
+    
     def test_backend_shell(self):
+        ''' A defined method for the console unit tests cases''' 
         self.assertTrue(BackendShell.do_EOF(self, True))
         self.assertTrue(BackendShell.do_quit(self, True))
 

6958b8ed52533a023aafe8712d382143c299b724
Hope_Davids <hopedavids@gmail.com>
2023-03-13 12:54:50 -0400

    modified base_model

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -28,7 +28,7 @@ class BaseModel:
         turn to translate the the attribute name into a string
         '''
         class_name = type(self).__name__
-        return f"[{class_name}]{self.id}{self.created_at}]"
+        return f"[{class_name}] ({self.id}) ({self.created_at})]"
 
     def save(self):
         '''The save method for the BaseModel class which determines

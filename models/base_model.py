c70fbb4596c22af2bd9a97caca6f98fb1c1fda0f
Hope_Davids <hopedavids@gmail.com>
2023-03-13 11:51:01 -0400

    base_model object

--- models/base_model.py
+++ models/base_model.py
@@ -9,19 +9,26 @@ class BaseModel:
         will be used by the airbnb application
     '''
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         # The initializer method for the BaseModel class
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
-        self.__dict__.update(kwargs)
+        if kwargs:  # updating the attributes of the base model
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = datetime.now().isoformat()
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         ''' The __str__ method for the BaseModel class which
         turn to translate the the attribute name into a string
         '''
         class_name = type(self).__name__
-        return f"[{class_name}]{self.id}={self.created_at}]"
+        return f"[{class_name}]{self.id}{self.created_at}]"
 
     def save(self):
         '''The save method for the BaseModel class which determines

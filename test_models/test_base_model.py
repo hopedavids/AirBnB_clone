d8732c31dc36da8c37b93e28ca389406efdd1ba3
Hope_Davids <hopedavids@gmail.com>
2023-03-13 12:23:53 -0400

    modified test base model

--- test_models/test_base_model.py
+++ test_models/test_base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-from ..models.base_model import BaseModel
+sys.path.append('../')
+from models.base_model import BaseModel
 
 my_model = BaseModel()
 my_model.name = "My First Model"

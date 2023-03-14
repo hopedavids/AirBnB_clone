9632116d843f70e88ce4480b1afa7b901e6ab549
Hope_Davids <hopedavids@gmail.com>
2023-03-14 12:45:29 +0000

    added a create method to console

--- console.py
+++ console.py
@@ -2,13 +2,28 @@
 
 import sys
 import cmd
-
+import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """This is a the backend interactive shell interface"""
     intro = "<<Backend Interactive Console. Type help or ? to list commands>>"
     prompt = "(hbnb) "
     file = None
+    classes = {'BaseModel': BaseModel}
+
+    def do_create(self, line):
+        """Creates a new instances of BaseModel, saves it (to the JSON file) and prints the id"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        class_name = args[0]
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[class_name]()
+        new_instance.save()
+        print(new_instance.id) 
 
     def do_EOF(self, line):
         # An End of File command to close the open file

d9e920d6042950e97c9be28e3ce9068831881e05
Hope_Davids <hopedavids@gmail.com>
2023-03-15 09:07:53 +0000

    updated filestorage with the amenity module

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,8 @@ import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
+from models.city import City
+from models.amenity import Amenity
 
 
 

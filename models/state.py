27a998d40d1da9ba8eecae70ae0dbcf5cdd7fb43
Hope_Davids <hopedavids@gmail.com>
2023-03-15 08:48:17 +0000

    added state file

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the State Class that inherits from Basemodel
+"""
+
+from models.base_model import BaseModel
+
+class State:
+    """This is the State class that define """
+
+    name = ""

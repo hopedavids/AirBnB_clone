442da64381d17dad513a96c50a53254bad8fb8ff
Hope_Davids <hopedavids@gmail.com>
2023-03-15 09:28:35 +0000

    added review to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-
+from models.review import Review
 
 
 class FileStorage:

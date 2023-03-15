0864d14ec981980278b770360f0d57ec384d613b
Hope_Davids <hopedavids@gmail.com>
2023-03-15 08:29:52 +0000

    modified user module

--- models/user.py
+++ models/user.py
@@ -5,15 +5,16 @@ This module contains the User class that inherits from BaseModel.
 from models.base_model import BaseModel
 
 
-class User:
+class User(BaseModel):
     """ This is the user class that inherits from the BaseModel Attribute """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
-    def __init__(self, email, password, first_name, last_name):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+    # def __init__(self, email, password, first_name, last_name):
+    #     super().__init__(self, *args, **kwargs)
+    #     self.email = email
+    #     self.password = password
+    #     self.first_name = first_name
+    #     self.last_name = last_name

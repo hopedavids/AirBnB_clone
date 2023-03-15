b6e4e3ef94140e16db608c6b624766c6422fb0de
Hope_Davids <hopedavids@gmail.com>
2023-03-15 09:28:55 +0000

    review module

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the Review Class that inherits from Basemodel
+"""
+
+from models.base_model import BaseModel
+
+class Review:
+    """This is the State class that define """
+    place_id = ""
+    user_id = ""
+    text = ""

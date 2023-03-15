d12463953427d04ffc37029b543f52370705c986
Hope_Davids <hopedavids@gmail.com>
2023-03-15 08:56:30 +0000

    the city class module

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the City Class that inherits from Basemodel
+"""
+
+from models.base_model import BaseModel
+
+class City:
+    """This is the City class that define """
+    state_id = ""
+    name = ""

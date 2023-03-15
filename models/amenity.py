e2461c547312f694ce44e474889ffa63a49cd474
Hope_Davids <hopedavids@gmail.com>
2023-03-15 09:08:34 +0000

    amenity module

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the Amenity Class that inherits from Basemodel
+"""
+
+from models.base_model import BaseModel
+
+class Amenity:
+    """This is the Amenity class that define """
+
+    name = ""
